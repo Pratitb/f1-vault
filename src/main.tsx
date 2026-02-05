f08a6e0afa233b47cd5f80755661f1618efdb5ff
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2026-02-05 16:29:22 +0530

    feat: hash router instead of browser router

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { GlobalContext } from './context/GlobalContext.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
@@ -12,9 +12,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <GlobalContext>
-        <BrowserRouter basename='/f1-vault'>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </GlobalContext>
     </QueryClientProvider>
   </StrictMode>,

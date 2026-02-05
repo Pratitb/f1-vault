5049804f7f7beff1fbc14e27711d2e848d1bc7b4
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2026-02-05 15:25:34 +0530

    feat: react responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,12 @@ export default {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        soft: `0 1px 2px rgba(0,0,0,0.05), 0 8px 16px rgba(0,0,0,0.1)`,
+      },
       colors: {
         primary: '#475569',
         red: '#ff1e02',
-        lightRed: '#E57373'
       }
     },
   },

7b99b444c14660fde793ef79f18c2b26d1e25da3
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2026-02-11 17:49:22 +0530

    fix: banner height

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import type { BannerProps } from "../utils/types"
 
 const Banner = ({ name }: BannerProps) => {
     return (
-        <div className='bg-red capitalize text-5xl p-8 rounded-lg flex items-center justify-center break-all md:justify-start text-gray-100 font-semibold italic'>{name}</div>
+        <div className='bg-red capitalize text-5xl p-4 rounded-lg flex items-center justify-center break-all md:justify-start text-gray-100 font-semibold italic'>{name}</div>
     )
 }
 

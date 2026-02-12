df1fa63e14291c5917d2835ba0e35a49fd93cc27
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2026-02-12 13:15:00 +0530

    fix: logo size for small screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../utils/assets"
 const Header = () => {
     return (
         <div className="flex justify-center pb-6">
-            <img src={Logo} alt='' className="" />
+            <img src={Logo} alt='' className="max-w-28" />
         </div>
     )
 }
